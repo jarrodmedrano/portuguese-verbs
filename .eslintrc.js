176f3fb4a00af4aa0038dd718eea0e192bceda06
Jarrod Medrano <jmedran@gmail.com>
2023-01-19 19:21:18 -0700

    install vitest

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: ['next'],
+  settings: {
+    next: {
+      rootDir: 'packages/client',
+    },
+  },
+  root: true,
+  rules: {
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+  },
+};
