d8e80973ad3c80f220fbd7b262bca84bc7683157
Jarrod Medrano <jmedran@gmail.com>
2023-02-11 14:45:33 -0700

    add styles for dark table

--- packages/client/src/components/VerbTable.tsx
+++ packages/client/src/components/VerbTable.tsx
@@ -51,11 +51,11 @@ const VerbTable: React.FC<VerbTableProps> = ({ verb, mood, filters }: VerbTableP
   }, [filters, data]);
 
   return (
-    <>
+    <div className={`dark:bg-gray-700  dark:text-white `}>
       {isLoading ? (
         <Loading />
       ) : (
-        <div className="flex justify-center">
+        <div className="flex justify-center dark:bg-gray-700 dark:text-white">
           {filters.map((filter) => {
             return (
               <div className="form-check" key={`filter-${filter}`}>
@@ -108,11 +108,35 @@ const VerbTable: React.FC<VerbTableProps> = ({ verb, mood, filters }: VerbTableP
       <div className="p-4 sm:ml-64">
         <div className="rounded-lg border-2 border-dashed border-gray-200 p-4 dark:border-gray-700">
           <div className="mb-4 flex items-center justify-center rounded bg-gray-50 dark:bg-gray-800">
-            <Table columns={columns} rows={rows} should_export={false} />
+            <Table
+              columns={columns}
+              rows={rows}
+              should_export={false}
+              styling={{
+                top: {
+                  elements: {
+                    search:
+                      'block w-full rounded-sm border border-transparent border-gray-300 bg-gray-50 p-4 pl-10 text-sm text-gray-900 hover:outline-none focus:border-transparent focus:border-blue-500 focus:outline-none focus:ring-0 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500',
+                  },
+                },
+                table_head: {
+                  table_row: 'dark:bg-slate-500 dark:text-white',
+                  table_data: 'dark:bg-slate-500 dark:text-white',
+                },
+                main: 'dark:bg-gray-800 dark:text-white',
+                footer: {
+                  page_numbers: 'bg-gray-50 dark:bg-gray-800 dark:text-white',
+                },
+                table_body: {
+                  main: 'dark:bg-gray-700 dark:text-white',
+                  table_row: 'dark:bg-gray-700 dark:text-white hover:bg-gray-200 dark:hover:bg-slate-600',
+                },
+              }}
+            />
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
