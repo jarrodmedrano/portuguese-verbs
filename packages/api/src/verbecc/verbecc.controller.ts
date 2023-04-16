74363095d25035debace103148dd94d8e408fc89
Jarrod Medrano <jmedran@gmail.com>
2023-04-16 17:42:04 -0600

    fix ports

--- packages/api/src/verbecc/verbecc.controller.ts
+++ packages/api/src/verbecc/verbecc.controller.ts
@@ -8,6 +8,8 @@ export type Verb = {
 };
 
 export const getConjugation = async ({ verb, mood }: { verb: string; mood?: string }): Promise<Verb> => {
+  // eslint-disable-next-line no-console
+  console.log('api connected to ', process.env.VERBECC_API);
   const response = await axios.get(`${process.env.VERBECC_API}/conjugate/pt/${verb}?mood=${mood}`);
   const data = response.data;
   return data;
