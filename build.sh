dd56ed87e2229efba0e362a9694f78604953f8dd
Jarrod Medrano <jmedran@gmail.com>
2023-05-29 19:53:04 -0500

    modify cloudformation template

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 BUCKET_NAME=$(aws cloudformation describe-stacks --stack-name compose-pipeline --query "Stacks[0].Outputs[?OutputKey=='S3BucketName'].OutputValue" --output text)
 
-zip -r compose-bundle.zip .
+git archive --format=zip HEAD -o compose-bundle.zip
 aws s3 cp compose-bundle.zip s3://$BUCKET_NAME/compose-bundle.zip
