39e112476caf04969c8e4de8a4c9c73b3c933298
Jarrod Medrano <jmedran@gmail.com>
2023-08-23 07:51:33 -0500

    update vpc region

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,7 +9,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = var.region
   access_key = var.TF_VAR_AWS_ACCESS_KEY_ID
   secret_key = var.TF_VAR_AWS_SECRET_ACCESS_KEY
 }
@@ -79,12 +79,12 @@ module "public_alb" {
   security_groups   = [module.public_alb_security_group.security_group_id]
 }
 
-module "route53_private_zone" {
-  source            = "./modules/route53"
-  internal_url_name = var.internal_url_name
-  alb               = module.internal_alb.internal_alb
-  vpc_id            = module.vpc.vpc_id
-}
+# module "route53_private_zone" {
+#   source            = "./modules/route53"
+#   internal_url_name = var.internal_url_name
+#   alb               = module.internal_alb.internal_alb
+#   vpc_id            = module.vpc.vpc_id
+# }
 
 module "ecr" {
   source           = "./modules/ecr"
