05ed2bccbf2ad9b5287eb52c9b57134a8592406b
Jarrod Medrano <jarrod.medrano@changehealthcare.com>
2023-09-01 15:38:39 -0500

    remo0ve unused files & change terraform setup

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,116 +1,137 @@
-terraform {
-  required_version = ">= 0.12.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
+# Configure AWS provider
+provider "aws" {
+  region = var.region
+}
+
+# Create ECR repository for images
+resource "aws_ecr_repository" "repo" {
+  name = var.app_name
+}
+
+# Create ECS cluster
+resource "aws_ecs_cluster" "cluster" {
+  name = "${var.app_name}-cluster"
+}
+
+# Create ECS task definition
+resource "aws_ecs_task_definition" "task" {
+  family = "${var.app_name}-task"
+
+  container_definitions = jsonencode([
+    {
+      name  = "verbecc"
+      image = "${aws_ecr_repository.repo.repository_url}:verbecc"
+      portMappings = [{
+        containerPort = 8000
+        hostPort      = 8000
+      }]
+    },
+    {
+      name  = "api"
+      image = "${aws_ecr_repository.repo.repository_url}:api"
+      portMappings = [{
+        containerPort = 4000
+        hostPort      = 4000
+      }]
+    },
+    {
+      name  = "client"
+      image = "${aws_ecr_repository.repo.repository_url}:client"
+      portMappings = [{
+        containerPort = 3000
+        hostPort      = 3000
+      }]
+    },
+    {
+      name  = "nginx"
+      image = "nginx:stable"
+      portMappings = [{
+        containerPort = 80
+        hostPort      = 80
+      }]
     }
+  ])
+
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  memory                   = 2048
+  cpu                      = 1024
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+}
+
+# Create ECS service
+resource "aws_ecs_service" "service" {
+  name            = "${var.app_name}-service"
+  cluster         = aws_ecs_cluster.cluster.id
+  task_definition = aws_ecs_task_definition.task.arn
+  desired_count   = 1
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.tg.arn
+    container_name   = "nginx"
+    container_port   = 80
+  }
+
+  network_configuration {
+    subnets          = [aws_subnet.public_1.id, aws_subnet.public_2.id]
+    assign_public_ip = true
   }
 }
 
-provider "aws" {
-  region = var.region
-  access_key = var.TF_VAR_AWS_ACCESS_KEY_ID
-  secret_key = var.TF_VAR_AWS_SECRET_ACCESS_KEY
+# Create ALB target group
+resource "aws_lb_target_group" "tg" {
+  name        = "${var.app_name}-tg"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = aws_vpc.main.id
 }
 
-locals {
-  internal_alb_target_groups = { for service, config in var.microservice_config : service => config.alb_target_group if !config.is_public }
-  public_alb_target_groups   = { for service, config in var.microservice_config : service => config.alb_target_group if config.is_public }
+# Create ALB
+resource "aws_lb" "lb" {
+  name               = "${var.app_name}-lb"
+  internal           = false
+  load_balancer_type = "application"
+  subnets            = [aws_subnet.public_1.id, aws_subnet.public_2.id]
 }
 
-module "iam" {
-  source   = "./modules/iam"
-  app_name = var.app_name
+# Create VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
 }
 
-module "vpc" {
-  source             = "./modules/vpc"
-  app_name           = var.app_name
-  env                = var.env
-  cidr               = var.cidr
-  availability_zones = var.availability_zones
-  public_subnets     = var.public_subnets
-  private_subnets    = var.private_subnets
-  region = var.region
+# Create public subnets
+resource "aws_subnet" "public_1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "${var.region}a"
 }
 
-module "internal_alb_security_group" {
-  source        = "./modules/security-group"
-  name          = "${lower(var.app_name)}-internal-alb-sg"
-  description   = "${lower(var.app_name)}-internal-alb-sg"
-  vpc_id        = module.vpc.vpc_id
-  ingress_rules = var.internal_alb_config.ingress_rules
-  egress_rules  = var.internal_alb_config.egress_rules
+resource "aws_subnet" "public_2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "${var.region}b"
 }
 
-module "public_alb_security_group" {
-  source        = "./modules/security-group"
-  name          = "${lower(var.app_name)}-public-alb-sg"
-  description   = "${lower(var.app_name)}-public-alb-sg"
-  vpc_id        = module.vpc.vpc_id
-  ingress_rules = var.public_alb_config.ingress_rules
-  egress_rules  = var.public_alb_config.egress_rules
+# Create ECS service IAM role
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "${var.app_name}-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Sid    = ""
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+    }]
+  })
 }
 
-module "internal_alb" {
-  source            = "./modules/alb"
-  name              = "${lower(var.app_name)}-internal-alb"
-  subnets           = module.vpc.private_subnets
-  vpc_id            = module.vpc.vpc_id
-  target_groups     = local.internal_alb_target_groups
-  internal          = true
-  listener_port     = 80
-  listener_protocol = "HTTP"
-  listeners         = var.internal_alb_config.listeners
-  security_groups   = [module.internal_alb_security_group.security_group_id]
-}
-
-module "public_alb" {
-  source            = "./modules/alb"
-  name              = "${lower(var.app_name)}-public-alb"
-  subnets           = module.vpc.public_subnets
-  vpc_id            = module.vpc.vpc_id
-  target_groups     = local.public_alb_target_groups
-  internal          = false
-  listener_port     = 80
-  listener_protocol = "HTTP"
-  listeners         = var.public_alb_config.listeners
-  security_groups   = [module.public_alb_security_group.security_group_id]
-}
-
-module "route53_private_zone" {
-  source            = "./modules/route53"
-  internal_url_name = var.internal_url_name
-  alb               = module.internal_alb.internal_alb
-  vpc_id            = module.vpc.vpc_id
-}
-
-module "ecr" {
-  source           = "./modules/ecr"
-  app_name         = var.app_name
-  ecr_repositories = var.app_services
-}
-
-module "ecs" {
-  source                      = "./modules/ecs"
-  app_name                    = var.app_name
-  app_services                = var.app_services
-  account                     = var.account
-  region                      = var.region
-  service_config              = var.microservice_config
-  ecs_task_execution_role_arn = module.iam.ecs_task_execution_role_arn
-  vpc_id                      = module.vpc.vpc_id
-  private_subnets             = module.vpc.private_subnets
-  public_subnets              = module.vpc.public_subnets
-  public_alb_security_group   = module.public_alb_security_group
-  internal_alb_security_group = module.internal_alb_security_group
-  internal_alb_target_groups  = module.internal_alb.target_groups
-  public_alb_target_groups    = module.public_alb.target_groups
-}
-
-// create ssh keypair for ec2 instance
-resource "aws_key_pair" "terraform_ssh_key" {
-  key_name   = "terraform_key_rsa"
-  public_key = file("~/.ssh/aws/terraform_key_rsa.pub")
+# Attach required policies to ECS role
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
