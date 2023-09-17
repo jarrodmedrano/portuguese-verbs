eeb9ff1476038b1aa86c035476b2b121c0223c99
Jarrod Medrano <jmedran@gmail.com>
2023-09-17 11:25:16 -0500

    fixed api call

--- apps/client/pages/_app.tsx
+++ apps/client/pages/_app.tsx
@@ -20,7 +20,7 @@ const App = ({ Component, pageProps }: AppProps) => {
 
   const [trpcClient] = useState(() =>
     trpc.createClient({
-      url: `/api/trpc`,
+      url: `${publicRuntimeConfig.trpc_api}/trpc`,
     }),
   );
 
