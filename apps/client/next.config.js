6bc02e2fdd79d7b2408966556860aeef86084103
Jarrod Medrano <jmedran@gmail.com>
2023-09-30 15:32:43 -0500

    added redirects in next config

--- apps/client/next.config.js
+++ apps/client/next.config.js
@@ -1,8 +1,31 @@
 const path = require('path');
+const isDevelopment = process.env.NODE_ENV !== 'production';
+const server = process.env.NEXT_PUBLIC_TRPC_API;
+
+// eslint-disable-next-line no-console
+console.log('why isnt this working', process.env.NEXT_PUBLIC_TRPC_API);
+const rewritesConfig = isDevelopment
+  ? [
+      {
+        source: '/trpc/:path*',
+        destination: `${server}/bees/:path*`,
+      },
+    ]
+  : [
+      {
+        source: '/trpc/:path*',
+        destination: `${server}/bees/:path*`,
+      },
+    ];
 
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  async rewrites() {
+    // eslint-disable-next-line no-console
+    console.log('the server', rewritesConfig);
+    return rewritesConfig;
+  },
   output: 'standalone',
   experimental: {
     outputFileTracingRoot: path.join(__dirname, '../../'),
@@ -21,21 +44,21 @@ module.exports = {
   env: {
     trpc_api: process.env.NEXT_PUBLIC_TRPC_API,
   },
-  async headers() {
-    return [
-      {
-        source: '/api/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value:
-              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-          },
-        ],
-      },
-    ];
-  },
+  // async headers() {
+  //   return [
+  //     {
+  //       source: '/api/:path*',
+  //       headers: [
+  //         { key: 'Access-Control-Allow-Credentials', value: 'true' },
+  //         { key: 'Access-Control-Allow-Origin', value: '*' },
+  //         { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+  //         {
+  //           key: 'Access-Control-Allow-Headers',
+  //           value:
+  //             'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+  //         },
+  //       ],
+  //     },
+  //   ];
+  // },
 };
