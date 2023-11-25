d6139c77bb673062d276d40f0528dd769036d066
Jarrod Medrano <jmedran@gmail.com>
2023-11-25 09:28:14 -0700

    change localhost 8080

--- apps/client/utils/trpc.ts
+++ apps/client/utils/trpc.ts
@@ -1,19 +1,6 @@
 import { httpBatchLink } from '@trpc/client';
 import { createTRPCNext } from '@trpc/next';
 import type { TRPCRouter } from 'api';
-// function getBaseUrl() {
-//   if (typeof window !== 'undefined')
-//     // browser should use relative path
-//     return '';
-//   if (process.env.VERCEL_URL)
-//     // reference for vercel.com
-//     return `https://${process.env.VERCEL_URL}`;
-//   if (process.env.RENDER_INTERNAL_HOSTNAME)
-//     // reference for render.com
-//     return `http://${process.env.RENDER_INTERNAL_HOSTNAME}:${process.env.PORT}`;
-//   // assume localhost
-//   return `http://localhost:${process.env.PORT ?? 4000}`;
-// }
 export const trpc = createTRPCNext<TRPCRouter>({
   config() {
     return {
@@ -23,7 +10,7 @@ export const trpc = createTRPCNext<TRPCRouter>({
            * If you want to use SSR, you need to use the server's full URL
            * @link https://trpc.io/docs/ssr
            **/
-          url: `http://localhost:4000/trpc`,
+          url: `${process.env.NEXT_PUBLIC_TRPC_API}/api/trpc`,
           // You can pass any HTTP headers you wish here
           async headers() {
             return {
--- iac/app/ecs_app.tf
+++ iac/app/ecs_app.tf
@@ -142,6 +142,10 @@ resource "aws_ecs_task_definition" "app" {
         {
           name  = "VERBECC_PORT"
           value = "${tostring(local.verbecc.container.port)}"
+        },
+        {
+          name  = "testerT"
+          value = "test"
         }
       ]
       portMappings = [
